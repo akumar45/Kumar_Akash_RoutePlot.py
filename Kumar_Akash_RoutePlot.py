42b851de46365feb3c8f99d4f6a5b9f13c780daa
akumar45 <136478779+akumar45@users.noreply.github.com>
2023-06-13 15:59:15 +0100

    Add files via upload

---
+++ Kumar_Akash_RoutePlot.py
@@ -0,0 +1,97 @@
+def route(cell):
+    print()
+    print("Grid Layout")
+    print()
+    print('   -¦---|---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print(' 12 ¦ '+cell[1201]+' ¦ '+cell[1202]+' ¦ '+cell[1203]+' ¦ '+cell[1204]+' ¦ '+cell[1205]+' ¦ '+cell[1206]+' ¦ '+cell[1207]+' ¦ '+cell[1208]+' ¦ '+cell[1209]+' ¦ '+cell[1210]+' ¦ '+cell[1211]+' ¦ '+cell[1212]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print(' 11 ¦ '+cell[1101]+' ¦ '+cell[1102]+' ¦ '+cell[1103]+' ¦ '+cell[1104]+' ¦ '+cell[1105]+' ¦ '+cell[1106]+' ¦ '+cell[1107]+' ¦ '+cell[1108]+' ¦ '+cell[1109]+' ¦ '+cell[1110]+' ¦ '+cell[1111]+' ¦ '+cell[1112]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print(' 10 ¦ '+cell[1001]+' ¦ '+cell[1002]+' ¦ '+cell[1003]+' ¦ '+cell[1004]+' ¦ '+cell[1005]+' ¦ '+cell[1006]+' ¦ '+cell[1007]+' ¦ '+cell[1008]+' ¦ '+cell[1009]+' ¦ '+cell[1010]+' ¦ '+cell[1011]+' ¦ '+cell[1012]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print('  9 ¦ '+cell[901]+' ¦ '+cell[902]+' ¦ '+cell[903]+' ¦ '+cell[904]+' ¦ '+cell[905]+' ¦ '+cell[906]+' ¦ '+cell[907]+' ¦ '+cell[908]+' ¦ '+cell[909]+' ¦ '+cell[910]+' ¦ '+cell[911]+' ¦ '+cell[912]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print('  8 ¦ '+cell[801]+' ¦ '+cell[802]+' ¦ '+cell[803]+' ¦ '+cell[804]+' ¦ '+cell[805]+' ¦ '+cell[806]+' ¦ '+cell[807]+' ¦ '+cell[808]+' ¦ '+cell[809]+' ¦ '+cell[810]+' ¦ '+cell[811]+' ¦ '+cell[812]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print('  7 ¦ '+cell[701]+' ¦ '+cell[702]+' ¦ '+cell[703]+' ¦ '+cell[704]+' ¦ '+cell[705]+' ¦ '+cell[706]+' ¦ '+cell[707]+' ¦ '+cell[708]+' ¦ '+cell[709]+' ¦ '+cell[710]+' ¦ '+cell[711]+' ¦ '+cell[712]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print('  6 ¦ '+cell[601]+' ¦ '+cell[602]+' ¦ '+cell[603]+' ¦ '+cell[604]+' ¦ '+cell[605]+' ¦ '+cell[606]+' ¦ '+cell[607]+' ¦ '+cell[608]+' ¦ '+cell[609]+' ¦ '+cell[610]+' ¦ '+cell[611]+' ¦ '+cell[612]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print('  5 ¦ '+cell[501]+' ¦ '+cell[502]+' ¦ '+cell[503]+' ¦ '+cell[504]+' ¦ '+cell[505]+' ¦ '+cell[506]+' ¦ '+cell[507]+' ¦ '+cell[508]+' ¦ '+cell[509]+' ¦ '+cell[510]+' ¦ '+cell[511]+' ¦ '+cell[512]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print('  4 ¦ '+cell[401]+' ¦ '+cell[402]+' ¦ '+cell[403]+' ¦ '+cell[404]+' ¦ '+cell[405]+' ¦ '+cell[406]+' ¦ '+cell[407]+' ¦ '+cell[408]+' ¦ '+cell[409]+' ¦ '+cell[410]+' ¦ '+cell[411]+' ¦ '+cell[412]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print('  3 ¦ '+cell[301]+' ¦ '+cell[302]+' ¦ '+cell[303]+' ¦ '+cell[304]+' ¦ '+cell[305]+' ¦ '+cell[306]+' ¦ '+cell[307]+' ¦ '+cell[308]+' ¦ '+cell[309]+' ¦ '+cell[310]+' ¦ '+cell[311]+' ¦ '+cell[312]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print('  2 ¦ '+cell[201]+' ¦ '+cell[202]+' ¦ '+cell[203]+' ¦ '+cell[204]+' ¦ '+cell[205]+' ¦ '+cell[206]+' ¦ '+cell[207]+' ¦ '+cell[208]+' ¦ '+cell[209]+' ¦ '+cell[210]+' ¦ '+cell[211]+' ¦ '+cell[212]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print('  1 ¦ '+cell[101]+' ¦ '+cell[102]+' ¦ '+cell[103]+' ¦ '+cell[104]+' ¦ '+cell[105]+' ¦ '+cell[106]+' ¦ '+cell[107]+' ¦ '+cell[108]+' ¦ '+cell[109]+' ¦ '+cell[110]+' ¦ '+cell[111]+' ¦ '+cell[112]+' ¦')
+    print('   -¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦---¦-')
+    print("      1   2   3   4   5   6   7   8   9   10  11  12 ")
+    print()
+
+x = 1
+
+cell = [' ']*1314
+
+bottom_border = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+top_border = [1300, 1301, 1302, 1303, 1304, 1305, 1306, 1307, 1308, 1309, 1310, 1311, 1312, 1313]
+left_border = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200]
+right_border = [113, 213, 313, 413, 513, 613, 713, 813, 913, 1013, 1113, 1212]
+
+outside = 0
+
+while x == 1:
+    
+    outside = 0
+    
+    try:
+        route1 = input("Please enter the filename of the next route instructions file, or enter STOP to finish: ")
+        if route1 == "STOP":
+            break
+        else:
+            directions = open(route1)
+    except:
+        print()
+        print("File not found")
+        print()
+    else:
+        cell = [' ']*1314
+        directions = directions.read()
+        directions1 = directions.split()
+        current_position = int(directions1[0]) + int(directions1[1])*100
+        cell[int(current_position)] = 'x'
+        directions2 = directions1[3:]
+        directions3 = [int(-100) if x == 'S' else x for x in directions2]
+        directions4 = [int(+100) if x == 'N' else x for x in directions3]
+        directions5 = [int(+1) if x == 'E' else x for x in directions4]
+        directions6 = [int(-1) if x == 'W' else x for x in directions5]
+        for plot in directions6:
+            current_position = current_position + plot
+            cell[int(current_position)] = 'x'
+        for position in left_border or right_border or top_border or bottom_border:
+            if cell[position] == 'x':
+                outside = 1
+        if outside == 1:
+            print()
+            print('error: The route is outside of the grid')
+            print()
+        if outside == 0:
+            route(cell)
+            a = int(directions1[0])
+            b = int(directions1[1])
+            print("Coordinates")
+            print()
+            print('('+str(a)+','+' '+str(b)+')')
+            for f in directions2:
+                if f == 'S':
+                    b = b -1
+                if f == 'N':
+                    b = b +1
+                if f == 'W':
+                    a = a -1
+                if f == 'E':
+                    a = a +1
+                print('('+str(a)+','+' '+str(b)+')')
+            print()
+        x = 1
